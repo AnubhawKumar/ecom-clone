424c2c340945c835ccba598f5055788cce81779a
AnubhawKumar <92393354+AnubhawKumar@users.noreply.github.com>
2022-03-21 12:11:32 +0530

    second commit

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,25 +1,28 @@
 import React, { useEffect, useState } from "react";
+import data from "../../localDataBase/localDB";
 import Product from "../Product/Product";
 // import data from '../../localDataBase/localDB';
 import "./List.css";
 
 const List = () => {
-  const [data, setData] = useState(null);
+
+  const[result,setResult] = useState(data);
+  // const [data, setData] = useState(null);
 
 
-  useEffect(() => {
-    fetch("http://demo4500454.mockable.io/product")
-      .then((res) => res.json())
-      .then((result) => {
-        console.log(result);
-        setData(result.products);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch("http://demo4500454.mockable.io/product")
+  //     .then((res) => res.json())
+  //     .then((result) => {
+  //       console.log(result);
+  //       setData(result.products);
+  //     });
+  // }, []);
 
 
   return (
     <div className="each-item-wrapper">
-      {data && data.map((eachItem,index) => (
+      {result && result.products.map((eachItem,index) => (
           <Product item ={eachItem} key={index} />
       ))}
     </div>
